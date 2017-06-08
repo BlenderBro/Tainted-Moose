c9221ecf10cb0d2e4e68fcad730eac2360a0e1cf
vagrant <vagrant@homestead>
2017-06-08 17:16:18 +0000

    Initial scafolding

---
+++ resources/views/index.blade.php
@@ -0,0 +1,123 @@
+@include('partials.head')
+
+<!-- Header -->
+<header>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<img class="img-responsive" src="img/sab_profile.png" alt="">
+				<div class="intro-text">
+					<span class="name">My projects</span>
+					<hr class="star-light">
+					<span class="skills">Copywriter Dreamer Doer</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+
+<!-- Portfolio Grid Section -->
+<section id="portfolio">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h2>Portfolio</h2>
+				<hr class="star-primary">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4 portfolio-item">
+				<a href="/portfolio-item" class="portfolio-link" data-toggle="modal">
+					<div class="caption">
+						<div class="caption-content">
+							<i class="fa fa-search-plus fa-3x"></i>
+						</div>
+					</div>
+					<img src="img/portfolio/Project_1_ING.png" class="img-responsive" alt="">
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- About Section -->
+<section class="success" id="about">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h2>Who am I?</h2>
+				<hr class="star-light">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4 col-lg-offset-2">
+				<p>A positive and energetic word enthuziast, who still believes in stories. An analytical mind, with a creative spirit and strong sense of humor, in a constant search for knowledge.</p>
+			</div>
+			<div class="col-lg-4">
+				<p>Curious in nature and easily adaptable. [...] To gain full access to my XP and skill levels, feel free to ask for a face2face meeting, alongside with my CV. </p>
+			</div>
+			<div class="col-lg-8 col-lg-offset-2 text-center">
+				<a href="#" class="btn btn-lg btn-outline">
+					<i class="fa fa-download"></i> Check my LinkedIn
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Contact Section -->
+<section id="contact">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h2>Contact Me</h2>
+				<hr class="star-primary">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2">
+				<!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+				<!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+				<form name="sentMessage" id="contactForm" novalidate>
+					<div class="row control-group">
+						<div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Name</label>
+							<input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
+							<p class="help-block text-danger"></p>
+						</div>
+					</div>
+					<div class="row control-group">
+						<div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Email Address</label>
+							<input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
+							<p class="help-block text-danger"></p>
+						</div>
+					</div>
+					<div class="row control-group">
+						<div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Phone Number</label>
+							<input type="tel" class="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number.">
+							<p class="help-block text-danger"></p>
+						</div>
+					</div>
+					<div class="row control-group">
+						<div class="form-group col-xs-12 floating-label-form-group controls">
+							<label>Message</label>
+							<textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
+							<p class="help-block text-danger"></p>
+						</div>
+					</div>
+					<br>
+					<div id="success"></div>
+					<div class="row">
+						<div class="form-group col-xs-12">
+							<button type="submit" class="btn btn-success btn-lg">Send</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
+
+@include('partials.footer')
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,75 @@
+<!-- Footer -->
+<footer class="text-center">
+	<div class="footer-above">
+		<div class="container">
+			<div class="row">
+				<div class="footer-col col-md-4">
+					<h3>Location</h3>
+					<p>Bucharest
+						<br>ROMANIA</p>
+				</div>
+				<div class="footer-col col-md-4">
+					<h3>Find me at</h3>
+					<ul class="list-inline">
+						<li>
+							<a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+						</li>
+						<li>
+							<a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+						</li>
+						<li>
+							<a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+						</li>
+						<li>
+							<a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+						</li>
+						<li>
+							<a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+						</li>
+					</ul>
+				</div>
+				<div class="footer-col col-md-4">
+					<h3>Juicy info</h3>
+					<p>I simply love caffeine! Let's talk more over coffee. Email me at sabrinavlasceanu@gmail.com.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer-below">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					Copyright &copy; Your Website 2016
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+<div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+	<a class="btn btn-primary" href="#page-top">
+		<i class="fa fa-chevron-up"></i>
+	</a>
+</div>
+
+
+<!-- jQuery -->
+<script src="{{ asset('/js/jquery.min.js')}}"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="{{ asset('/js/bootstrap.min.js')}}"></script>
+
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+<!-- Contact Form JavaScript -->
+<script src="{{ asset('/js/jqBootstrapValidation.js')}}"></script>
+<script src="{{ asset('/js/contact_me.js')}}"></script>
+
+<!-- Theme JavaScript -->
+<script src="{{ asset('/js/freelancer.min.js')}}"></script>
+
+</body>
+
+</html>
