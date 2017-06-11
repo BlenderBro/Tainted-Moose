32717111b1a4f201a1eeb8d26fe9d95f9fb42c0c
vlad_dobrescu <vlad.s.dobrescu@gmail.com>
2017-06-12 00:18:28 +0300

    securing assets

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -55,20 +55,20 @@
 
 
 <!-- jQuery -->
-<script src="{{ asset('/js/jquery.min.js')}}"></script>
+<script src="{{ secure_asset('/js/jquery.min.js')}}"></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src="{{ asset('/js/bootstrap.min.js')}}"></script>
+<script src="{{ secure_asset('/js/bootstrap.min.js')}}"></script>
 
 <!-- Plugin JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 
 <!-- Contact Form JavaScript -->
-<script src="{{ asset('/js/jqBootstrapValidation.js')}}"></script>
-<script src="{{ asset('/js/contact_me.js')}}"></script>
+<script src="{{ secure_asset('/js/jqBootstrapValidation.js')}}"></script>
+<script src="{{ secure_asset('/js/contact_me.js')}}"></script>
 
 <!-- Theme JavaScript -->
-<script src="{{ asset('/js/freelancer.min.js')}}"></script>
+<script src="{{ secure_asset('/js/freelancer.min.js')}}"></script>
 
 
 
